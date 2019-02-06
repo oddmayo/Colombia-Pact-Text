18a7f3f37f1f780269722479fdd449903693fba1
FoxHound112263 <camayorquina@unal.edu.co>
2019-02-06 17:08:15 -0500

    Better comments per task

--- code/analisis descriptivo.R
+++ code/analisis descriptivo.R
@@ -1,38 +1,41 @@
+# para instalar paquetes modificar linea 142 sys.sleep(5.0)
 trace(utils:::unpackPkgZip, edit=TRUE)
-# casa
+# Directorio casa
 directorio <- 'C:\\Users\\CamiloAndrés\\Desktop\\Colombia-Pact-Text'
-# DNP
+# Directorio DNP
 directorio <- 'C:\\Users\\ScmayorquinS\\OneDrive - Departamento Nacional de Planeacion\\DIDE\\2019\\Data Science Projects\\Colombia-Pact-Text'
-
 
 # Funciones preprocesamiento y t-sne
 source(paste0(directorio,'/code/funciones.R'))
 
-
 # Cargar paquetes
 paquetes <- c('dplyr','readxl','data.table','magrittr','RTextTools','tictoc','ggplot2','tm',
               'ClusterR','factoextra','FactoMineR','beepr','quanteda','Rtsne','deldir','sp',
-              'rgeos','reshape','tidyr','tidytext','stringr','wordcloud','tidyverse'
+              'rgeos','reshape','tidyr','tidytext','stringr','wordcloud','tidyverse','packcircles',
+              'viridis'
 )
 lapply(paquetes, require, character.only = TRUE)
 
-#################################
-# Preprocesamiento Excel portal #
-#################################
+#----------------------------------------------------------------------------------------------------------------------
+# Preprocesamiento Excel portal
+#----------------------------------------------------------------------------------------------------------------------
 
+# Cargo respuestas de las personas
 base <- read_excel(paste0(directorio,'/data/propuestas.xlsx'))
-base <- base[,-1]
+base <- base[,-1] # Eliminar columna sin nombre
+# Dejar respuestas solo de innovación
 base <- base %>% filter(Título == '¿Cómo podrían las entidades públicas innovar para mejorar procesos y servicios?')
 
-# Stop words
+# Cargo archivo con stop words
 sw <- readLines(paste0(directorio,"\\data\\entrada\\stop_words_spanish.txt"))
 
-# stop words acidionales
+# Stop words adicionales que surgen después de una primera iteración
 sw_a <- readLines(paste0(directorio,"\\data\\entrada\\stop_words_adicionales.txt"))
 
-# todas las stop words
+# Todas las stop words en un objeto
 all_stopwords <- unique(c(sw,sw_a))
 
+# Preprocesamiento del texto
 base_clean <- as.data.frame(sapply(base, function(x) preproctext(x)))
 base_clean %<>% mutate_if(is.factor,as.character)
 
@@ -40,32 +43,36 @@ base_clean %<>% mutate_if(is.factor,as.character)
 base_clean <- as.data.frame(sapply(base_clean, function(x) RemoveStopwordsFromText(x,all_stopwords)))
 base_clean %<>% mutate_if(is.factor,as.character)
 
-# Texto analizar - descripcion
+# Texto para analizar - "Descripción" de la base de datos
 descripcion <- base_clean$Descripción
 
-# cargar texto a lematizar
+# Cargar texto a "lematizar"
 adicional <- read.table(paste0(directorio,"\\data\\entrada\\lematizacion-adicional.txt"),sep ='\t',header = F,encoding = 'UTF-8',colClasses = 'character')
-adicional <- separate(data = adicional,col = 'V1',into = c('V1','V2'))
+adicional <- separate(data = adicional,col = 'V1',into = c('V1','V2')) # Separar en dos columnas
 
-# Lematización
+# "Lematización" - Buscar primera palabra en el archivo y reemplazarla por la versión corregida
 for (i in 1:nrow(adicional)) {
   descripcion <- gsub(adicional[i,'V1'],adicional[i,'V2'],descripcion)
 }
 
+# Obtener cada caracter del texto
 lista_palabras <- unlist(strsplit(descripcion,split = ' ',fixed = T))
+# Ordenar en orden decreciente
 lista_palabras <- sort(table(lista_palabras),decreasing = T)
 
+# Eliminar palabras adicionales tras una segunda iteración
 test <- as.data.frame(lista_palabras)
 test <- test[which(test$lista_palabras!='co'),]
 test <- test[which(test$lista_palabras!='gov'),]
 test <- test[which(test$lista_palabras!='anos'),]
 test <- test[which(test$lista_palabras!='ano'),]
+# Primeras 20 palabras para realizar barplot
 test2 <- test[1:20,]
 
+#----------------------------------------------------------------------------------------------------------------------
+# Barplot básico
+#----------------------------------------------------------------------------------------------------------------------
 
-#################
-# Basic barplot #
-#################
 theme_update(plot.title = element_text(hjust = 0.5))
 p<-ggplot(data=test2, aes(x=test2$lista_palabras, y=test2$Freq)) +
   geom_bar(stat="identity",fill='steelblue') +
@@ -76,65 +83,59 @@ p<-ggplot(data=test2, aes(x=test2$lista_palabras, y=test2$Freq)) +
   xlab('Palabras') + ylab('Frecuencia')
 p
 
+#----------------------------------------------------------------------------------------------------------------------
+# Wordcloud
+#----------------------------------------------------------------------------------------------------------------------
 
-#############
-# Wordcloud #
-#############
-
+# Dibujar usando paquete wordcloud
 set.seed(1234)
 x11()
 wordcloud(words = test$lista_palabras, freq = test$Freq, min.freq = 1,
           max.words=100, random.order=FALSE, rot.per=0.35, 
           colors=brewer.pal(8, "Dark2"))
 
-####################
-# Circular packing #
-####################
+#----------------------------------------------------------------------------------------------------------------------
+# Circular packing
+#----------------------------------------------------------------------------------------------------------------------
 
-# libraries
-library(packcircles)
-library(ggplot2)
-library(viridis)
-# Create data
-
+# Datos
 data=test2
 
-# Generate the layout. This function return a dataframe with one line per bubble. 
-# It gives its center (x and y) and its radius, proportional of the value
+# Generar diseño, se retorna un dataframe con una línea por burbuja
+# Retorna el centro (x e y) y su radio, proprocional al valor
 packing <- circleProgressiveLayout(data$Freq, sizetype='area')
 
-# We can add these packing information to the initial data frame
+# Añadir packing como columna al data frame original
 data = cbind(data, packing)
 
-# Check that radius is proportional to value. We don't want a linear relationship, since it is the AREA that must be proportionnal to the value
+# Comprobar que el radio sea proporcional al valor, no se quiere una relación lineal
 plot(data$radius, data$Freq)
 
-# The next step is to go from one center + a radius to the coordinates of a circle that
-# is drawn by a multitude of straight lines.
-dat.gg <- circleLayoutVertices(packing, npoints=20)
+# Ir de un centro + un radio a las coordenadas de la burbuja que es dibujado por un montón de líneas rectas
+dat.gg <- circleLayoutVertices(packing, npoints=20) # Creo que entre más npoints más resolución tiene la burbuja
 
-# Make the plot
+# Dibujar con ggplot
 x11()
 ggplot() + 
   
-  # Make the bubbles
+  # Construir burbujas
   geom_polygon(data = dat.gg, aes(x, y, group = id, fill=as.factor(id)), colour = "black", alpha = 0.6) +
   
-  # Add text in the center of each bubble + control its size
+  # Añadir texto al centro de cada burbuja y controlar su tamaño
   scale_fill_manual(values = viridis(nrow(data))) + 
   geom_text(data = data, aes(x, y, size=5, label = lista_palabras)) +
   #scale_color_gradient(colours=rainbow(4))+
   scale_size_continuous(range = c(1,4)) +
   
-  # General theme:
+  # Tema general
   theme_void() + 
   theme(legend.position="none") +
   coord_equal()
 
 
-#########################
-# Análisis con bigramas #
-#########################
+#----------------------------------------------------------------------------------------------------------------------
+# Análisis con nmgramas sobre el texto en general
+#----------------------------------------------------------------------------------------------------------------------
 
 myDfm <- tokens(descripcion) %>%
   tokens_ngrams(n = 3) %>%
@@ -144,9 +145,9 @@ bigramas <- topfeatures(myDfm,n = 30)
 
 
 
-#######################################
-# Respuestas que contienen solo salud #
-#######################################
+#----------------------------------------------------------------------------------------------------------------------
+# Respuestas que contienen solo salud + análisis de ngramas
+#----------------------------------------------------------------------------------------------------------------------
 
 salud <- base_clean[grepl("salud", base$Descripción),]
 
