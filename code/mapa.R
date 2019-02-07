7bab4a6714669341748e64151b99311c5c59ad94
FoxHound112263 <camayorquina@unal.edu.co>
2019-02-07 09:56:37 -0500

    Better comments and dealing with encoding

--- code/mapa.R
+++ code/mapa.R
@@ -1,68 +1,72 @@
-library(sp)
-library(RColorBrewer)
-library(ggplot2)
-library(maptools)
-library(scales)
-library(readxl)
+# Directorio DNP
+directorio <- 'C:\\Users\\ScmayorquinS\\OneDrive - Departamento Nacional de Planeacion\\DIDE\\2019\\Data Science Projects\\Colombia-Pact-Text'
 
-# DNP
-directorio <- 'C:\\Users\\cmayorquin\\Desktop\\portal presidencia'
-
-
+# Cargar paquetes
 paquetes <- c('dplyr','readxl','data.table','magrittr','RTextTools','tictoc','ggplot2','tm',
               'ClusterR','factoextra','FactoMineR','beepr','quanteda','Rtsne','deldir','sp',
-              'rgeos','reshape','tidyr','tidytext','stringr','wordcloud','tidyverse','utf8'
+              'rgeos','reshape','tidyr','tidytext','stringr','wordcloud','tidyverse','utf8',
+              'RColorBrewer','maptools','scales'
 )
-
 
 lapply(paquetes, require, character.only = TRUE)
 
+#----------------------------------------------------------------------------------------------------------------------
+# Preprocesamiento para el mapa
+#----------------------------------------------------------------------------------------------------------------------
+
 # Base con ordenamiento de departamentos de acuerdo a id del shape
-DatosPau <- read_excel("C:\\Users\\cmayorquin\\Desktop\\portal presidencia\\data\\mapa\\datoos.xlsx", range = "A1:B33")
+DatosPau <- read_excel(paste0(directorio, "\\data\\mapa\\datoos.xlsx"), range = "A1:B33")
 
-# Base de datos 
-base <- read_excel(paste0(directorio,'/data/propuestas.xlsx'))
+# Base con respuesats de personas
+base <- read_excel(paste0(directorio, '/data/propuestas.xlsx'))
 base <- base[,-1]
-base <- base %>% filter(Título == '¿Cómo podrían las entidades públicas innovar para mejorar procesos y servicios?')
+base <- base %>% filter(TÃ­tulo == 'Â¿CÃ³mo podrÃ­an las entidades pÃºblicas innovar para mejorar procesos y servicios?')
 
-# Cambiar todos los 'no hay registro' y 'Bogotá' por Cundinamarca
-base$Departamento <- gsub('no hay registro','Cundinamarca',base$Departamento)
-base$Departamento <- gsub('Bogota D.C.','Cundinamarca',base$Departamento)
+# Cambiar todos los 'no hay registro' y 'BogotÃ¡' por Cundinamarca
+base$Departamento <- gsub('no hay registro', 'Cundinamarca', base$Departamento)
+base$Departamento <- gsub('Bogota D.C.', 'Cundinamarca', base$Departamento)
 
 # Dejar solo departamentos de Colombia
-base <- base[ ! base$Departamento %in% c('Braga','Cochabamba','Grevenmacher'), ]
-# Reducir a usuarios y departamento
-base <- data.frame(usuario=base$Username,depto=base$Departamento)
-# Dejar total por cada depto en orden
+base <- base[ ! base$Departamento %in% c('Braga', 'Cochabamba', 'Grevenmacher'), ]
+# Reducir dataframe a solo usuarios y departamento
+base <- data.frame(usuario=base$Username, depto=base$Departamento)
+
+# Dejar total de personas por cada depto en orden
 base2 <- unique(base)
 base2 <- as.data.frame(base2[,-1])
 base2 <- as.data.frame(table(base2))
 base2 <- base2[order(-base2$Freq),]
-# Igualo los nombres de las bases a mergear
-base2$base2 <- gsub('Valle del Cauca','Valle',base2$base2)
-base2$base2 <- gsub('Narino','Nariño',base2$base2)
-#base2$base2 %in% DatosPau$depto
 
-# matching names para ambos data frames
+# Igualo los nombres de las bases a mergear
+base2$base2 <- gsub('Valle del Cauca', 'Valle',base2$base2)
+base2$base2 <- gsub('Narino', 'NariÃ±o', base2$base2)
+
+# Matching colnames para ambos data frames
 colnames(base2) <- c('depto','Freq')
-# hail dplyr
+
+# Merge entre ambos data frames - hail dplyr
 definitiva <- full_join(DatosPau, base2,"depto")
-definitiva$Cuartil <- NULL
+definitiva$Cuartil <- NULL # Innecesario
 
 # Cambiar NAs por ceros
 definitiva$Freq[is.na(definitiva$Freq)] <- 0
 
-####################
-# Contrucción mapa #
-####################
-ohsCol2 <- readShapeSpatial('C:\\Users\\cmayorquin\\Desktop\\portal presidencia\\data\\mapa\\geo_export_98f694ca-e5a7-40e0-a59f-523ec74b2f25.shp')
+#----------------------------------------------------------------------------------------------------------------------
+# ContrucciÃ³n mapa
+#----------------------------------------------------------------------------------------------------------------------
+
+# Leer shapefile - funciÃ³n deprecated pero funciona
+ohsCol2 <- readShapeSpatial(paste0(directorio,'\\data\\mapa\\geo_export_98f694ca-e5a7-40e0-a59f-523ec74b2f25.shp'))
+# Convertir objeto a dataframe - FunciÃ³n podrÃ­a ser deprecated en un futuro
 ohsColI2 <- fortify(ohsCol2)
+# id para cada departamento
 grupo2 <- data.frame(id = unique(ohsColI2[ , c("id")]))
-#grupo2 <- head(grupo2,-1)
+# Convertir en factor los valores a mostrar en leyenda
 grupo2[ , "Porcentaje"] <- factor(definitiva$Freq,levels = c("31","15","5","2",'1','0'))
+# Merge entre dataframe, id y los valores de la leyenda
 ohsColI2 <- merge(ohsColI2, grupo2, by = "id")
 
-
+# Mapa con ggplot
 mapColDep <- ggplot() +
   geom_polygon(data=ohsColI2, aes(x=long, y=lat, group = group,
                                   fill = Porcentaje), colour ="black", size = 0.1) +
